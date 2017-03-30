543ab4bb62fb6c2852a46157b40eb1fafdab8e9e
Nguyen Van Quyen <quyennv.bg@gmail.com>
2017-03-31 01:02:18 +0700

    first commit

---
+++ src/js/template/NavLeft.js
@@ -0,0 +1,3 @@
+/**
+ * Created by nvquyen on 3/31/17.
+ */
---
+++ src/js/template/NavTop.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+
+class NavTop extends Component {
+
+    render() {
+        return (
+            <div className="App">
+            </div>
+        );
+    }
+}
+
+export default NavTop;
